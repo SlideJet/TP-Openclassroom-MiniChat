410470d9ab121b846f7b0cd2aaa6075962510d59
SlideJet <23705544+SlideJet@users.noreply.github.com>
2018-03-27 09:59:45 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>	
+    <meta charset="utf-8">	
+    <title>Accès au Mini-chat</title>	
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <body style="text-align: center;">	
+        <br />	
+        <img src="minichat.png" alt="Openclassrooms" >	
+        <h1>Veuillez entrer un pseudo pour accèder au mini-chat.</h1>	
+        <br />	
+        <form action="cookieid.php" method="post">		
+            <p>            
+                <input type="text" name="pseudo" placeholder="Pseudo" required="" />            
+                <input type="submit" value="Valider" />        
+            </p>	
+         </form>	
+         <br />	
+         <p>Cette page a été créer dans le but de stocker un cookie nommer 'pseudo' à des fins éducatives.</p>	
+         <p>Merci de votre compréhension.</p>	
+         <br />	<br />	
+         <i>*Ce site utilise des cookies pour vous assurer la meilleure expérience sur notre site..</i>
+ </body>
+ <footer>	
+ <img src="Logo_OpenClassrooms.png" alt="Openclassrooms" style="width: 100px;">	<p>Copyright <a href="https://openclassrooms.com/membres/yanouschpetry">SlideJet</a> 2018 © - Openclassrooms</p>   	
+ </footer>
+ </html>
