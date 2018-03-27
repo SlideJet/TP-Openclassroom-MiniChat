30a7aeb705ac246af3e120358e9cd97856bf99af
SlideJet <23705544+SlideJet@users.noreply.github.com>
2018-03-27 10:09:02 +0200

    Add files via upload

---
+++ cookieid.php
@@ -0,0 +1,6 @@
+<?php
+//je créer un cookie contenant le pseudo de l'utilisateur.
+    setcookie('pseudo', $_POST['pseudo'], time() + 24*3600, null, null, false, true);
+//Puis je redirige sur le minichat.
+    header('Location: minichat1.php');
+?>
