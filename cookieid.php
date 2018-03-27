dfc169c6877e4e30c8fb6f614d9f64194cf35139
SlideJet <23705544+SlideJet@users.noreply.github.com>
2018-03-27 10:08:50 +0200

    Delete cookieid.php

--- cookieid.php
+++
@@ -1,8 +0,0 @@
-<?php
-//je créer un cookie contenant le pseudo de l'utilisateur.    
-setcookie('pseudo', $_POST['pseudo'], time() + 24*3600, null, null, false, true);
-
-//Puis je redirige sur le minichat.    
-header('Location: minichat1.php');
-
-?>
